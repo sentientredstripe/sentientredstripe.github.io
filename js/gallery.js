cf7cd57f293df19cec5fea21e44963bde34f7a15
sentientredstripe <sentientredstripe@users.noreply.github.com>
2015-05-02 14:43:37 -0400

    Played around with Gallery function in Materialize

---
+++ js/gallery.js
@@ -0,0 +1,7 @@
+(function($){
+  $(function(){ 
+$(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });  
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
