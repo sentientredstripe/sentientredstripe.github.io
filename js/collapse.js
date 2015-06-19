3503fa752e7c840387120ec315d3c7095a99932c
(Sentient)Redstripe <sentientredstripe@users.noreply.github.com>
2015-06-19 10:43:51 -0400

    Started another thing

---
+++ js/collapse.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){ 
+  $(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+  });
+  })(jQuery); // end of jQuery name space
