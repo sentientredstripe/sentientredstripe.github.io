3ed3aaecd232d22f92ed612b3139dac927ea1083
Ian Smithh <droidlocalhost@gmail.com>
2016-04-30 03:13:59 +0000

    cover page be like live man

--- js/init.js
+++ js/init.js
@@ -2,8 +2,12 @@
   $(function(){
 
     $('.button-collapse').sideNav();
-
+    $(document).ready(function(){
+      $('.parallax').parallax();
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
+
+        
 
